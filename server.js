5d68fef69496e095e5b1104512d5007ae2beda42
Siam Smart Solutions <adiruj-s@hotmail.com>
2022-11-30 15:08:07 +0700

    2

--- server.js
+++ server.js
@@ -2,13 +2,16 @@ import express from 'express';
 import mysqlcon from './db_env/db_connection.js';
 
 import http from 'http';
+
+const port = 3000;
 const app = express()
 
+const server = http.createServer(app);
 
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
 
-app.listen(3000, () => {
+server.listen(port, () => {
   console.log('Start server at port 3000.')
 })
